a7eedd3ecad289b679c0ae2db9c592f571622dad
Ole Peter Smith <ole.ufg@gmail.com>
2016-04-25 01:53:24 -0300

    oo

--- Sivent2/temp.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-//display_startup_errors(true);
-//display_errors(true);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-include("index.php");
-
-?>
