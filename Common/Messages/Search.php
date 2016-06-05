64b9f9c2d5b4ca1d68db3804cd67a8edc240893e
Ole Peter Smith <ole.ufg@gmail.com>
2016-06-05 19:07:43 -0300

    oooo

---
+++ Common/Messages/Search.php
@@ -0,0 +1,8 @@
+array
+(
+   "IncludeAll_Inactive_Message" => array
+   (
+      "Name" => "Pesquisa Ativa",
+      "Name_UK" => "Search Active",
+    ),
+);
---
+++ Sivent2/System/Events/Data.Selection.php
@@ -0,0 +1,27 @@
+array
+(     
+   "Selection" => array
+   (
+      "Name" => "Seleção",
+      "Title" => "Seleção de Candidatos",
+      "Name_UK" => "Selection",
+      "Title_UK" => "Selection of Candidates",
+      
+      "SelectCheckBoxes"  => 2,
+      
+      "Sql" => "ENUM",
+      "Values" => array("Não","Sim"),
+      "Values_UK" => array("No","Yes"),
+      
+      "Public"   => 0,
+      "Person"   => 0,
+      "Admin"    => 2,
+      "Friend" => 1,
+      "Coordinator" => 2,
+      
+      "Default" => 1,
+      "Compulsory" => FALSE,
+      "Search" => FALSE,
+      "Compulsory"  => TRUE,
+   ),
+);
