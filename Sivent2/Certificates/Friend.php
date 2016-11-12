02e7cb162493922f927cff3af9d89df9f28bef91
Ole Peter Smith <ole.ufg@gmail.com>
2016-11-12 12:25:38 -0200

    adding. now complete.

---
+++ Sivent2/Certificates/Friend.php
@@ -0,0 +1,83 @@
+<?php
+
+include_once("Certificates/Access.php");
+include_once("Certificates/Validate.php");
+include_once("Certificates/Latex.php");
+include_once("Certificates/Generate.php");
+include_once("Certificates/Read.php");
+include_once("Certificates/Mail.php");
+include_once("Certificates/Code.php");
+include_once("Certificates/Verify.php");
+include_once("Certificates/Handle.php");
+
+
+class Certificates_Friend extends Certificates_Verify
+{
+     //*
+    //* function Certificates_Friend_Tables, Parameter list: $friend,$event=array()
+    //*
+    //* Creates Friend certificates table.
+    //*
+
+    function Certificates_Friend_Tables($friend,$event=array())
+    {
+        $where=$this->UnitWhere(array("Friend" => $friend[ "ID" ]));
+        if (!empty($event)) { $where[ "Event" ]=$event[ "ID" ]; }
+        
+        $certs=
+            $this->Sql_Select_Hashes
+            (
+               $where,
+               array(),
+               "Type,Name"
+            );
+
+        if (empty($certs)) { return ""; }
+
+        $n=1;
+
+        $table=array();
+        foreach ($this->MyHash_HashesList_Key($certs,"Event") as $eventid => $eventcerts)
+        {
+            $event=$this->EventsObj()->Sql_Select_Hash(array("ID" => $eventid));
+
+            if (!$this->EventsObj()->Event_Certificates_Published($event)) { continue; }
+
+            $table=
+                array_merge
+                (
+                    $table,
+                    $this->Certificates_Table($friend,$event,$eventcerts)
+                );
+        }
+
+        return $table;
+    }
+                
+    //*
+    //* function Certificates_Friend_Tables_Html, Parameter list: $friend,$event=array()
+    //*
+    //* Creates Friend certificates table.
+    //*
+
+    function Certificates_Friend_Tables_Html($friend,$event=array())
+    {
+        $this->ItemData("ID");
+        $this->Actions("Show");
+        
+        $datas=array("Generate","Generated","Mailed","Type","Name","TimeLoad","Code",);
+        
+
+        return
+            $this->H(1,$this->MyLanguage_GetMessage("Certificate_Friend_Table_Title")).
+            $this->H(2,$friend[ "Name" ]).
+            $this->Html_Table
+            (
+               $this->Certificates_Table_Titles(),
+               $this->Certificates_Friend_Tables($friend,$event)
+            ).
+            "";
+    }
+}
+
+?>
---
+++ Sivent2/Certificates/Table.php
@@ -0,0 +1,127 @@
+<?php
+
+
+class Certificates_Table extends Certificates_Friend
+{
+    //*
+    //* function Certificate_Row, Parameter list: $n,$friend,$event,$cert,$datas=array()
+    //*
+    //* Creates Friend $event $cert row.
+    //*
+
+    function Certificate_Row($n,$friend,$event,$cert,$datas=array())
+    {
+        return
+            $this->MyMod_Items_Table_Row
+            (
+                0,
+                $n,
+                $cert,
+                $datas
+            );
+    }
+
+    
+    //*
+    //* function Certificates_Table, Parameter list: $datas,$friend,$event,$certs,$datas=array()
+    //*
+    //* Creates Friend $event $cert table.
+    //*
+
+    function Certificates_Table($friend,$event,$certs,$datas=array())
+    {
+        if (empty($datas)) { $datas=$this->Certificates_Table_Datas(); }
+        
+        $n=1;
+        
+        $table=array();
+        foreach ($certs as $cert)
+        {
+            if (!$this->Certificate_Verify($cert) ) { continue; }
+        
+            array_push
+            (
+                $table,
+                $this->Certificate_Row($n++,$friend,$event,$cert,$datas)
+            );
+        }
+
+        if (!empty($table))
+        {
+            array_unshift
+            (
+                $table,
+                $this->H(3,$this->GetRealNameKey($event,"Name"))
+            );
+            array_push
+            (
+                $table,
+                $this->Certificates_Table_SumRow($friend,$event,$certs)
+            );
+        }
+
+        return $table;
+    }
+    
+     //*
+    //* function Certificates_Table_SumRow, Parameter list: $friend,$event,$certs,$datas=array()
+    //*
+    //* Creates Friend $event certificates table sumrow.
+    //*
+
+    function Certificates_Table_SumRow($friend,$event,$certs,$datas=array())
+    {
+        if (empty($datas)) { $datas=$this->Certificates_Table_Datas(); }
+        
+        $cht=0;
+        foreach ($certs as $cert)
+        {
+            if (!$this->Certificate_Verify($cert) ) { continue; }
+            
+            if (!empty($cert[ "TimeLoad" ])) { $cht+=$cert[ "TimeLoad" ]; }
+        }
+
+        $pos=array_search("TimeLoad",$datas);
+        $row=
+            array
+            (
+                $this->MultiCell($this->ApplicationObj->Sigma,$pos+1,'r'),
+                $this->B($cht),
+                ""
+            );
+
+        return $row;
+    }
+
+    
+   //*
+    //* function Certificates_Table_Titles, Parameter list: $datas=array()
+    //*
+    //* Creates Friend certificates table title row.
+    //*
+
+    function Certificates_Table_Titles($datas=array())
+    {
+        if (empty($datas)) { $datas=$this->Certificates_Table_Datas(); }
+        
+        $titles=$this->GetDataTitles($datas);
+
+        return $titles;
+    }
+    
+    //*
+    //* function Certificates_Table_Datas, Parameter list: 
+    //*
+    //* Returns list of data to show for Friend/Event certificates tablew..
+    //*
+
+    function Certificates_Table_Datas()
+    {
+        $datas=array("No","Generate","Generated","Mailed","Type","Name","TimeLoad","Code",);
+
+        return $datas;
+    }
+        
+}
+
+?>
